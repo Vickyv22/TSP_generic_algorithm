c9c1ae62b817887badbbb5d37fe3e68510a92d4b
vignesh.v <90783935+Vickyv22@users.noreply.github.com>
2022-09-09 21:17:14 +0530

    Create splitting .py

---
+++ splitting .py
@@ -0,0 +1,21 @@
+def distance_between_cities(cities):
+    data = dict()
+    for index, value in enumerate(cities):
+        x1 = cities[index][0]
+        y1 = cities[index][1]
+        if index + 1 <= len(cities)-1:
+            x2 = cities[index+1][0]
+            y2 = cities[index+1][1]
+            xdiff = x2 - x1
+            ydiff = y2 - y1
+            dst = (xdiff*xdiff + ydiff*ydiff)** 0.5
+            data['Distance from city '+ str(index+1) +' to city ' + str(index+2)] = dst 
+        elif index + 1 > len(cities)-1:
+            x2 = cities[0][0]
+            y2 = cities[0][1]
+            xdiff = x2 - x1
+            ydiff = y2 - y1
+            dst = (xdiff*xdiff + ydiff*ydiff)** 0.5
+            data['Distance from city '+ str(index+1) + ' to city ' + str(index +2 -len(cities))] = dst
+              
+    return data
